20d3d91de20d7e58fcab1c696c3502d7afae5f2c
Smartfeld.ch <78407877+smartfeld-ch@users.noreply.github.com>
2021-07-19 14:36:39 +0200

    first commit

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-basic.showString("online2_m0002_pegelmesser!")
+basic.showString(":)")
+let schallpegel = input.soundLevel()
+basic.forever(function () {
+    schallpegel = input.soundLevel()
+    led.plotBarGraph(
+    schallpegel,
+    255
+    )
+    if (schallpegel > 150) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
